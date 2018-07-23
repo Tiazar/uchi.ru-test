2b94753affe2c7dcf323c9275c885d59c9aa44b0
Pavel Grudinkin <pgrudinkin@gmail.com>
2018-07-23 18:35:07 +0300

    request form, cotributors method

--- main/app/controllers/welcome_controller.rb
+++ main/app/controllers/welcome_controller.rb
@@ -1,2 +1,19 @@
 class WelcomeController < ApplicationController
+  before_action :permitted_params, only: [:contributors, :diplom]
+
+  def index
+
+  end
+
+  def contributors
+    # HTTParty.get("https://www.tinkoff.ru/api/v1/currency_rates/")
+    puts 'test'
+    contrubutors_res =  HTTParty.get("http://#{request.host}:4000/repo=#{@permitted_params[:repo]}&owner=#{@permitted_params[:owner]}&count=#{@permitted_params[:count]}")
+  end
+
+  protected
+
+    def permitted_params
+      @permitted_params = params.permit(:repo, :count, :owner).to_h
+    end
 end
